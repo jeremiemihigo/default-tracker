3157c9930480f3c2731a106533dccdab1eb9c058
unknown <grace1mihigo@gmail.com>
2025-08-22 12:04:57 +0200

    automatically

--- src/app/api/par120/datarefresh/route.ts
+++ src/app/api/par120/datarefresh/route.ts
@@ -22,7 +22,7 @@ export async function GET(request: NextRequest) {
 }
 export async function POST(request: NextRequest) {
   const token = request.cookies.get("access")?.value;
-  const link = `${lien_dt}/Automatically_set`;
+  const link = `${lien_dt}/automatically_set`;
   const res = await fetch(link, {
     method: "POST",
     headers: {
